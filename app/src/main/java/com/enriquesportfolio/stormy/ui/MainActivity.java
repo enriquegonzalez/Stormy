5520f9dfdcc9484f54b0a4b3fe2cfe1fa7f76631
Enrique <egonzalez78@gmail.com>
2015-04-29 16:30:28 -0700

    applied the parcelable interface

--- app/src/main/java/com/enriquesportfolio/stormy/ui/MainActivity.java
+++ app/src/main/java/com/enriquesportfolio/stormy/ui/MainActivity.java
@@ -38,6 +38,7 @@ import butterknife.OnClick;
 public class MainActivity extends ActionBarActivity  {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String DAILY_FORECAST = "Daily Forecast";
 
     private Forecast mForecast;
     @InjectView(R.id.timeLabel) TextView mTimeLabel;
@@ -260,6 +261,7 @@ public class MainActivity extends ActionBarActivity  {
     @OnClick (R.id.dailyButton)
     public  void startDailyActivity(View view) {
         Intent intent = new Intent(this, DailyForecastActivity.class);
+        intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/enriquesportfolio/stormy/weather/Day.java
+++ app/src/main/java/com/enriquesportfolio/stormy/weather/Day.java
@@ -1,5 +1,8 @@
 package com.enriquesportfolio.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -7,7 +10,7 @@ import java.util.TimeZone;
 /**
  * Created by Enrique on 4/27/15.
  */
-public class Day {
+public class Day implements Parcelable {
     private long mTime;
     private String mSummary;
     private double mTemperatureMax;
@@ -66,4 +69,40 @@ public class Day {
 
         return formatter.format(dateTime);
     }
+
+    @Override
+    public int describeContents() {
+        return 0; // Not using it
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperatureMax);
+        dest.writeString(mIcon);
+        dest.writeString(mTimezone);
+    }
+
+    private Day(Parcel in) {
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperatureMax = in.readDouble();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+    }
+
+    public Day () { }
+
+    public static final Creator<Day> CREATOR = new Creator<Day>() {
+        @Override
+        public Day createFromParcel(Parcel source) {
+            return new Day (source);
+        }
+
+        @Override
+        public Day[] newArray(int size) {
+            return new Day[size];
+        }
+    };
 }
