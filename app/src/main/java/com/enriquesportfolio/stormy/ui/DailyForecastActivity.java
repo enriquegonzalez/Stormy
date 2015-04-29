6d0a5de56c29c8faba10e0a8d483b356025fa831
Enrique <egonzalez78@gmail.com>
2015-04-29 13:25:28 -0700

    added custom listviews and a custom adapter for Days, created helper classes for getting icons by id and weekday

--- app/src/main/java/com/enriquesportfolio/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/enriquesportfolio/stormy/ui/DailyForecastActivity.java
@@ -8,19 +8,19 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
 import com.enriquesportfolio.stormy.R;
+import com.enriquesportfolio.stormy.adapters.DayAdapter;
+import com.enriquesportfolio.stormy.weather.Day;
 
 public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                daysOfTheWeek);
-        setListAdapter(adapter);
+        DayAdapter adapter = new DayAdapter(this, mDays);
     }
 
 }
--- app/src/main/java/com/enriquesportfolio/stormy/weather/Forecast.java
+++ app/src/main/java/com/enriquesportfolio/stormy/weather/Forecast.java
@@ -1,5 +1,7 @@
 package com.enriquesportfolio.stormy.weather;
 
+import com.enriquesportfolio.stormy.R;
+
 /**
  * Created by Enrique on 4/27/15.
  */
@@ -31,4 +33,45 @@ public class Forecast {
     public void setDailyForecast(Day[] dailyForecast) {
         mDailyForecast = dailyForecast;
     }
+
+    // helper method
+    public static int getIconId(String iconString) {
+
+        int iconId = R.drawable.clear_day;
+
+        if (iconString.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (iconString.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (iconString.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (iconString.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (iconString.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (iconString.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (iconString.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (iconString.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (iconString.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (iconString.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+
+        return iconId;
+
+    }
+
 }
