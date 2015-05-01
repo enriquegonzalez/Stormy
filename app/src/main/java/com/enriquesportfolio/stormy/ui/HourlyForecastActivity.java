e2760b94eeab8c7dfa91358c69a33542dfa1a4c0
Enrique <egonzalez78@gmail.com>
2015-05-01 14:41:58 -0700

    setup the hour adapter and linear layout manager, and tested

--- app/src/main/java/com/enriquesportfolio/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/enriquesportfolio/stormy/ui/HourlyForecastActivity.java
@@ -4,23 +4,46 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import com.enriquesportfolio.stormy.R;
+import com.enriquesportfolio.stormy.adapters.HourAdapter;
 import com.enriquesportfolio.stormy.weather.Hour;
 
 import java.util.Arrays;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class HourlyForecastActivity extends ActionBarActivity {
 
     private Hour[] mHours;
 
+    //get recycler view from layout
+    @InjectView(R.id.recyclerView) RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.inject(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        //create adapter
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        //add linear layout manager and set it as the layout manager for the recycler view
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        //performance optimization for fixed size recyclerViews like ours
+        mRecyclerView.setHasFixedSize(true);
+
 
     }
 
