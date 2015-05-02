f3590b85c477d1e22978e9d9f3d72981827976bf
Enrique <egonzalez78@gmail.com>
2015-05-01 21:45:30 -0700

    added an list item click listener to daily forecast and a toast message response

--- app/src/main/java/com/enriquesportfolio/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/enriquesportfolio/stormy/ui/DailyForecastActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.enriquesportfolio.stormy.R;
 import com.enriquesportfolio.stormy.adapters.DayAdapter;
@@ -33,4 +36,16 @@ public class DailyForecastActivity extends ListActivity {
         setListAdapter(adapter);
     }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+        String message = String.format("On %s the high will be %s and it will be %s", dayOfTheWeek, highTemp, conditions);
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+    }
 }
